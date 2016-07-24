d6c0e5cda715755dcbea8441ef3c504849ff9932
Idan Goldman <idan@goldman.work>
2016-07-24 01:36:57 -0400

    javascript event clicks for feature box and animated caret :D

--- resources/assets/js/side-kick/features/page-info.js
+++ resources/assets/js/side-kick/features/page-info.js
@@ -7,6 +7,24 @@ var pageInfoComponent = component( function application() {
         this.$node.html(
             template.render()
         );
+
+        // toggle feature box
+        this.on('.header', 'click', function( event ) {
+            $( event.currentTarget )
+                .toggleClass('close')
+                .parent()
+                    .children('.body')
+                        .slideToggle();
+        });
+
+        // focus click
+        this.on('.field-wrap .field', 'focus', function( event ) {
+            $( event.currentTarget ).parent().addClass('focus');
+        });
+        // blur click
+        this.on('.field-wrap .field', 'blur', function( event ) {
+            $( event.currentTarget ).parent().removeClass('focus');
+        });
     });
 });
 
