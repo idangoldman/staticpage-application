e29df561508255897e2beffcb718b406207835b9
Idan Goldman <idan@goldman.work>
2016-11-19 22:13:42 +0200

    rename of requirments.txt and add new variables to start_example

--- start_example.sh
+++ start_example.sh
@@ -2,6 +2,10 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export APP_SETTINGS="config.DevConfig"
 export CLEANCSS_BIN="node_modules/.bin/cleancss"
+export MAILCHIMP_APIKEY=""
+export MAILCHIMP_LIST_ID=""
+export GOOGLE_ANALYTICS_ID=""
+export ADDTHIS_PUBID=""
 
 
 venv/bin/python app.py
