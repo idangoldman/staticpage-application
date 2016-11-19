7088d83ebf227fa9c9f6da35e616ef2146686965
Idan Goldman <idan@goldman.work>
2016-11-20 00:53:09 +0200

    passing variables around

--- app/controllers/general.py
+++ app/controllers/general.py
@@ -1,19 +1,23 @@
 from flask import Flask, request
+from flask import current_app as app
 from flask_cors import CORS
+
 from mailchimp import Mailchimp
 from validate_email import validate_email
-from flask import current_app as app
+
+from pprint import pprint
+
 
 def mailchimp_subscribe(email):
     if email and validate_email(email):
-        api_key = ''  # API Key
+        api_key = app.config['MAILCHIMP_API_KEY']
         payload = {
-            "id": app.config['MAILCHIMP_MAILING_LIST_ID'],
+            "id": app.config['MAILCHIMP_LIST_ID'],
             "email": {
                 "email": email
             },
         }
-        mc = Mailchimp(app.config['MAILCHIMP_API_KEY'], True)
+        mc = Mailchimp(api_key, True)
         mc.call('lists/subscribe', payload)
         return True
     else:
--- config.py
+++ config.py
@@ -4,8 +4,10 @@ import os
 class Config(object):
     MONGODB_SETTINGS = {'DB': 'testing'}
     SECRET_KEY = "w2525ferg3456t354y"
+    MAILCHIMP_LIST_ID = os.environ['MAILCHIMP_LIST_ID']
+    MAILCHIMP_API_KEY = os.environ['MAILCHIMP_API_KEY']
+    GOOGLE_ANALYTICS_ID = os.environ['GOOGLE_ANALYTICS_ID']
+    ADDTHIS_PUBID = os.environ['ADDTHIS_PUBID']
 
 class DevConfig(Config):
     DEBUG = True
-    MAILCHIMP_MAILING_LIST_ID = 12
-    MAILCHIMP_API_KEY = '2134fsdcsdcsvcsdf'
