ffc47937ed3cd47514055e24ff1831f984d87242
Idan Goldman <idan@goldman.work>
2016-11-19 22:42:49 +0200

    fix js misspell error and add of css email validation

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,12 +1,14 @@
 jQuery( document ).ready(function ( $ ) {
     $('.newsletter').on('submit', function( event ) {
-        var emailAdress = $(this).children('.email').val().trim();
+        var emailEl = $(this).children('#email');
+        var emailAdress = emailEl.val().trim();
 
         function isEmail( address ) {
             return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test( address );
         }
 
         if ( ! emailAdress.length || ! isEmail( emailAdress ) ) {
+            emailEl.css({ 'border-color': '#f00' });
             event.preventDefault();
         }
     });
