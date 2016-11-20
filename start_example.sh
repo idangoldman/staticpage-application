2d8dc85a635f9d5889b69eb940627b9bee3a8adb
Idan Goldman <idan@goldman.work>
2016-11-20 21:46:30 +0200

    add autoprefixer support for style

--- start_example.sh
+++ start_example.sh
@@ -1,7 +1,11 @@
+FILE_PATH=`pwd`
+
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export APP_SETTINGS="config.DevConfig"
 export CLEANCSS_BIN="node_modules/.bin/cleancss"
+export AUTOPREFIXER_BIN="${FILE_PATH}/node_modules/.bin/postcss"
+
 
 export MAILCHIMP_API_KEY=""
 export MAILCHIMP_LIST_ID=""
