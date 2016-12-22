8a40e6466ed9e6ded128862b7eaba83b02ebc864
Idan Goldman <idan@goldman.work>
2016-12-22 18:29:07 +0200

    move general from features to old features and disable general feature from side kick

--- assets/js/side-kick/index.js
+++ assets/js/side-kick/index.js
@@ -1,8 +1,9 @@
 // import 'side-kick/storage';
+// import 'side-kick/features/general';
+
 import 'side-kick/api-calls';
 import 'side-kick/post-messaging';
 
 import 'side-kick/devices';
-import 'side-kick/features/general';
 import 'side-kick/features/content';
 import 'side-kick/features/design';
