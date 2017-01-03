d5df90d59e3b83b64bd4ef754f59419edabd47e1
Idan Goldman <idan@goldman.work>
2017-01-03 23:53:09 +0200

    I’m leaving the token aside for now

--- app/api/routes.py
+++ app/api/routes.py
@@ -43,15 +43,3 @@ def page(id):
         response_data = request_data
 
     return jsonify( { 'status': 'ok', 'data': response_data } )
-
-# @api.before_request
-# def before_api_request():
-#     if request.json is None:
-#         return errors.bad_request('Invalid JSON in body.')
-#     token = request.json.get('token')
-#     if not token:
-#         return errors.unauthorized('Authentication token not provided.')
-#     user = User.validate_api_token(token)
-#     if not user:
-#         return errors.unauthorized('Invalid authentication token.')
-#     g.current_user = user
