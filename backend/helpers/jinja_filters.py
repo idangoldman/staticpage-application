27cb67dbac6b44574f3bb73f3dd2706560e5720a
Idan Goldman <idan@goldman.work>
2017-01-11 21:54:13 +0200

    another <br> fix to jinja filter by removing it and using css line breaks

--- backend/helpers/jinja_filters.py
+++ backend/helpers/jinja_filters.py
@@ -1,7 +1,7 @@
 from flask import current_app
 from jinja2 import evalcontextfilter, Markup, escape
 import markdown as markdown_lib
-import re
+import re, jinja2
 
 
 # markdown
@@ -9,17 +9,3 @@ import re
 @evalcontextfilter
 def markdown(eval_ctx, value):
     return Markup(markdown_lib.markdown(value))
-
-# nl2br
-@current_app.template_filter()
-@evalcontextfilter
-def nl2br(eval_ctx, value):
-    if value == None:
-        value = ''
-
-    _paragraph_re = re.compile( r'(?:\r\n|\r|\n){2,}' )
-    result = u'\n\n'.join( u'%s' % p.replace('\n', '<br>\n' ) \
-        for p in _paragraph_re.split( escape( value ) ) )
-    if eval_ctx.autoescape:
-        result = Markup( result )
-    return result
