05c4455112c3577fd34fd2f19cdc3f7e27732846
Idan Goldman <idan@goldman.work>
2017-01-23 00:08:19 +0200

    bugfix

--- backend/auth/routes.py
+++ backend/auth/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, current_app
 from flask_login import login_user, logout_user, login_required, current_user
 
 from backend import db
