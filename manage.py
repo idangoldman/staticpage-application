245428d10e7f8ce5fd2fd119bc8484b12869ed2f
Idan Goldman <idan@goldman.work>
2018-09-14 00:52:55 +0300

    played around with some config variable names and structure, hope it helped..

--- manage.py
+++ manage.py
@@ -2,14 +2,9 @@ from werkzeug.contrib.fixers import ProxyFix
 import os
 
 from backend import create_app, db
-from backend.helpers import load_env_var
-
-
-# loading environmental variables
-load_env_var()
 
 # creating backend app
-app = create_app( os.getenv('FLASK_CONFIG') )
+app = create_app( os.environ['FLASK_CONFIG'] )
 app.wsgi_app = ProxyFix( app.wsgi_app )
 
 
