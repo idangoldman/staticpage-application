73826400fce6df40f5a03f56547237a9c12681a5
Idan Goldman <idan@goldman.work>
2018-11-20 20:59:34 +0200

    - added eslint - added script commands - move/update some dependencies around - improve webpack config - added yarn lock for now

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-var path = require("path");
+var path = require('path');
 
 module.exports = {
   watch: false,
   cache: false,
-  debug: true,
-  devtool: 'source-map',
+  debug: false,
+  devtool: 'none',
   entry: {
     'page-intervention': './frontend/js/page/intervention.js',
     'side-kick': './frontend/js/side-kick/index.js',
@@ -14,25 +14,25 @@ module.exports = {
     filename: '[name].js'
   },
   resolve: {
-    root: path.resolve(__dirname, 'frontend/js'),
-    modulesDirectories: [
-      'web_modules',
-      'node_modules',
-      'bower_components'
-    ],
-    alias: {
-      jquery: 'jquery/src/jquery'
-    }
+    root: path.resolve(__dirname, 'frontend/js')
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules)/,
         loader: 'babel',
         query: {
           presets: ['es2015']
         }
+      },
+      {
+        test: /(node_modules\/flightjs)/,
+        loader: 'imports-loader?$=jquery'
+      },
+      {
+        test: /(node_modules\/air-datepicker\/dist\/js)/,
+        loader: 'imports-loader?jQuery=jquery'
       },
       {
         test: /\.(njk|nunjucks)$/,
