36192acb85000934cd3b5ef401220a4c70ff263a
Idan Goldman <idan@goldman.work>
2018-12-27 20:54:00 +0200

    jump between pages, easy! :)

--- frontend/js/side-kick/features/components/manage-pages/pages.js
+++ frontend/js/side-kick/features/components/manage-pages/pages.js
@@ -3,7 +3,7 @@ import selectFieldComponent from 'side-kick/features/components/select-field';
 export default selectFieldComponent.mixin(function searchPreview() {
   this.updateField = function updateField(state, previousState) {
     if (previousState.value !== state.value) {
-      console.log('updateField', state.value);
+      window.top.location.href = state.value;
     }
   };
 });
